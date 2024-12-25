a96007850943a57d5c7c7abe8060efc222ccbad7
NAGASAI_CHILUKOTI <nagasaichilukoti71@gmail.com>
2024-12-25 07:17:31 +0530

    Update TextGenerateEffect.tsx

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "@/utils/cn";
 
@@ -11,42 +11,52 @@ export const TextGenerateEffect = ({
     className?: string;
 }) => {
     const [scope, animate] = useAnimate();
+    const [isClient, setIsClient] = useState(false); // To check if we're on the client side
+
     const wordsArray = words.split(" ");
+
     useEffect(() => {
-        animate(
-            "span",
-            {
-                opacity: 1,
-            },
-            {
-                duration: 2,
-                delay: stagger(0.2),
-            }
-        );
-    }, [scope.current]);
+        setIsClient(true); // This will run only on the client side
+    }, []);
+
+    useEffect(() => {
+        if (isClient) {
+            animate(
+                "span",
+                {
+                    opacity: 1,
+                },
+                {
+                    duration: 2,
+                    delay: stagger(0.2),
+                }
+            );
+        }
+    }, [isClient, scope.current, animate]);
 
     const renderWords = () => {
         return (
             <motion.div ref={scope}>
-                {wordsArray.map((word, idx) => {
-                    return (
-                        <motion.span
-                            key={word + idx}
-                            className={`${idx > 3 ? 'text-purple' : "dark:text-white text-black"}  opacity-0`}
-                        >
-                            {word}{" "}
-                        </motion.span>
-                    );
-                })
-                }
-            </motion.div >
+                {wordsArray.map((word, idx) => (
+                    <motion.span
+                        key={word + idx}
+                        className={`${idx > 3 ? 'text-purple' : "dark:text-white text-black"} opacity-0`}
+                    >
+                        {word}{" "}
+                    </motion.span>
+                ))}
+            </motion.div>
         );
     };
+
+    if (!isClient) {
+        return null; // Optionally render nothing or a loading spinner while waiting for client side render
+    }
 
     return (
         <div className={cn("font-bold", className)}>
             <div className="my-4">
-                <div className=" dark:text-white text-black  leading-snug tracking-wide">
+                <div className=" dark:text-white text-black leading-snug tracking-wide">
                     {renderWords()}
                 </div>
             </div>
