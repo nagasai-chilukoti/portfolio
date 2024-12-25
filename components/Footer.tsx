ceb6ef7bdc6460431d5d887ed53b99f3307bc654
NAGASAI_CHILUKOTI <nagasaichilukoti71@gmail.com>
2024-12-25 07:38:44 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,18 @@ import Link from 'next/link'
 const Footer = () => {
     return (
         <footer className='w-full pb-10 mb-[100px] md:mb-5 ' id='contact'>
-            
+            <div className='w-full absolute left-0 md:-bottom-72 min-h-96'>
+                <img src="/footer-grid.svg" alt="grid" className="w-full h-full opacity-50" />
+            </div>
             <div className="flex flex-col items-center " >
-                <a href="mailto:nagasaichilukoti71@gmail.com">
+                <h1 className='heading lg:max-w-[45vw]'>Empower <span className="text-purple">Your</span> Team with a Skilled Frontend Developer</h1>
+                <p className='text-white-200 md:mt-10 my-5 text-center md:text-md'>Let&apos;s connect and explore how my expertise in React, Next.js, and innovative web solutions can drive success for your projects. With a knack for quick learning and seamless adaptability, I am ready to contribute to your team&apos;s goals and create impactful digital experiences</p>
+                <a href="mailto:laibasif36@gmail.com">
                     <MagicButton title='Let&apos;s get in touch' icon={<FaLocationArrow />} position="right" />
                 </a>
             </div>
             <div className='flex mt-16 md:flex-row flex-col justify-between items-center '>
-                <p className='md:text-base text-sm md:font-normal font-light'>Copyright &copy; 2024 Nagasai</p>
+                <p className='md:text-base text-sm md:font-normal font-light'>Copyright &copy; 2024 Laiba</p>
                 <div className='flex items-center md:gap-3 gap-6'>
                     {socialMedia.map((profile) => (
                         <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 border border-black-300 rounded-lg'>
